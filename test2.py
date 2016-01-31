6fffdc727dca0e171a8d0ff7deabfc319c783d8d
Leroux <maxime.leroux@surycat.com>
2016-01-31 21:17:37 +0100

    premier test

--- test2.py
+++ test2.py
@@ -1,3 +1,18 @@
-hello
-coucouc 
-3 ieme ligne
+
+#! /usr/bin/env python
+#coding=utf-8
+ 
+import random
+
+print("hello");
+annee =input("annee ");
+print(annee);
+i = 1;
+while i < 10:
+	x=random.randint(1,10);
+	y=random.randint(1,10);
+	print(x,"x",y);
+	imput ()
+	z=x * y;
+	i+=1;
+	print(z);

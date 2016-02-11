647f5d2a2a896b068c1b5f503717559230568492
Maxime Leroux <maxime.leroux@optiflows.com>
2016-02-11 10:10:36 +0100

    test

--- test2.py
+++ test2.py
@@ -1,18 +1,30 @@
 
 #! /usr/bin/env python
-#coding=utf-8
- 
+# coding=utf-8
+
 import random
+import urllib2
+# int annee;
 
 print("hello");
-annee =input("annee ");
+
+annee =int(raw_input("Entrez un nombre: "))
 print(annee);
 i = 1;
-while i < 10:
+while i < 2:
 	x=random.randint(1,10);
 	y=random.randint(1,10);
 	print(x,"x",y);
-	imput ()
+	raw_input()
 	z=x * y;
 	i+=1;
 	print(z);
+
+
+class Rectangle:
+
+	def __init__(self, long = 0.0, larg = 0.0, coul = "blanc"):
+        	self.longueur = long
+        	self.largeur = larg
+        	self.couleur = coul
+

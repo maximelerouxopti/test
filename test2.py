15f5b9469fa8760ceaccc0fd20968a221688e13c
Maxime Leroux <maxime.leroux@optiflows.com>
2016-10-10 22:30:16 +0200

    meteo

--- test2.py
+++ test2.py
@@ -2,7 +2,23 @@
 # le rayon a été demandé au clavier à l'utilisateur.
 
 import math
+import json
+import requests
 
+
+def meteo(la_ville):
+    
+    api = "London"
+    return api
+
+
+def ma_req(la_ville):
+    my_key="959f090befe241b8c3993b96994a1cce"
+    requete="http://api.openweathermap.org/data/2.5/weather?q=" + la_ville + "&APPID=" + my_key   
+    return requete
+    
+
+    
 def perimetre_cercle(un_rayon):
     """Calculer le périmètre d'un cercle à partir de son rayon.
 	:param un_rayon: le rayon du cercle (positif)
@@ -15,14 +31,22 @@ def perimetre_cercle(un_rayon):
 def main():
     """Le programme principal."""
     # demander le rayon à l'utilisateur
-    saisie = input("Rayon du cercle : ")    # une chaîne de caractères
-    le_rayon = float(saisie)                # convertie en un nombre réel
+    ville = input("la ville : ")    # une chaîne de caractères
+    la_ville = str(ville)                # convertie en un nombre réel
+    mareq = la_ville +"test"
+    print (mareq)
 
+    # res = requests.get('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=959f090befe241b8c3993b96994a1cce')
+    # print(res.json())
     # calculer le périmètre
-    perimetre = perimetre_cercle(le_rayon)
+    # perimetre = perimetre_cercle(la_ville)
+    print("la ville est ", la_ville)
+    
+     = ma_req(la_ville)    
+    
 
     # afficher le périmètre à l'utilisateur
-    print("Le périmètre d'un cercle de rayon", le_rayon, "est", perimetre)
+    print("la ville est ", la_ville , "l'api", api)
 
 if __name__ == "__main__":
     main()

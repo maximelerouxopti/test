aef69f0e940c9bf85882e6672640c6fa74048242
Maxime Leroux <maxime.leroux@optiflows.com>
2016-10-10 15:41:53 +0200

    ajout

--- testapi.py
+++ testapi.py
@@ -1,1 +1,11 @@
-test
+#!/usr/bin/python
+
+import py
+import urllib2
+import os
+
+def hello(arg1, arg2):
+    print arg1, arg2
+
+
+# http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=959f090befe241b8c3993b96994a1cce

e0354a82c1ca0c49733e96f53c7b7037cdd07af6
Leroux <maxime.leroux@surycat.com>
2017-03-05 21:31:21 +0100

    ajout

--- testapi.py
+++ testapi.py
@@ -4,10 +4,40 @@ import py
 import urllib2
 import os
 
-def hello(arg1, arg2):
-    print arg1, arg2
+#def hello(arg1, arg2):
+#    print arg1, arg2
 
-je fais des tests
-pouet
+#je fais des tests
+#pouet
 
 # http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=959f090befe241b8c3993b96994a1cce
+# curl -0 -L "https://disqus.com/api/3.0/trends/listThreads.json?api_key=API_PUBLIC_KEY_HERE&callback=foo"
+
+def perimetre_cercle(un_rayon):
+    """Calculer le périmètre d'un cercle à partir de son rayon.
+        :param un_rayon: le rayon du cercle (positif)
+        :return le périmètre d'un cercle de rayon un_rayon
+    """
+    diametre = 2 * un_rayon
+    return math.pi * diametre
+
+def requete(ville):
+    villefc= 
+
+    return req
+ 
+
+
+def main():
+    """Le programme principal."""
+    # demander le rayon à l'utilisateur
+    ville = input("entrer la ville : ")    # une chaîne de caractères
+    mareq=requete
+    #le_rayon = float(saisie)                # convertie en un nombre réel
+    # calculer le périmètre
+    #perimetre = perimetre_cercle(le_rayon)
+    # afficher le périmètre à l'utilisateur
+    #print("Le périmètre d'un cercle de rayon", le_rayon, "est", perimetre)
+
+if __name__ == "__main__":
+    main()

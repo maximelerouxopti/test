a7d515331d64e3b903ea16511f23c5bb01b46ea8
Maxime Leroux <maxime.leroux@optiflows.com>
2017-03-08 22:15:56 +0100

    ajout

--- testapi.py
+++ testapi.py
@@ -13,14 +13,6 @@ import os
 # http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=959f090befe241b8c3993b96994a1cce
 # curl -0 -L "https://disqus.com/api/3.0/trends/listThreads.json?api_key=API_PUBLIC_KEY_HERE&callback=foo"
 
-def perimetre_cercle(un_rayon):
-    """Calculer le périmètre d'un cercle à partir de son rayon.
-        :param un_rayon: le rayon du cercle (positif)
-        :return le périmètre d'un cercle de rayon un_rayon
-    """
-    diametre = 2 * un_rayon
-    return math.pi * diametre
-
 def requete(ville):
     villefc= 
 
@@ -30,9 +22,20 @@ def requete(ville):
 
 def main():
     """Le programme principal."""
-    # demander le rayon à l'utilisateur
-    ville = input("entrer la ville : ")    # une chaîne de caractères
-    mareq=requete
+    try
+       page_json = urllib2.urlopen('http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=959f090befe241b8c3993b96994a1cce')
+    # Je lis la page
+       json_string = page_json.read()
+    # Je mets cette page dans un parseur
+       parsed_json = json.loads(json_string)
+    # Et je peux fermer ma page meteo, je n'en ai plus besoin
+       page_json.close()
+    except
+       print("ça marche pas")
+
+    
+   #    ville = input("entrer la ville : ")    # une chaîne de caractères
+#    mareq=requete
     #le_rayon = float(saisie)                # convertie en un nombre réel
     # calculer le périmètre
     #perimetre = perimetre_cercle(le_rayon)
